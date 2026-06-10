7b05ca69935d7b114d6c41e88a2a7784f2296721
fj123u <theo.girard.1807@gmail.com>
2026-06-10 10:43:54 +0200

    fix id

--- view/Concert/ShowConcert.php
+++ view/Concert/ShowConcert.php
@@ -14,7 +14,7 @@
         $idArtiste = $concert["idArtiste"];
         $idScene = $concert["idScene"];
         $date = $concert["date"];
-        $id = $concert["id"];
+        $id = $concert["idConcert"];
         $hDebut = $concert["heureDebut"];
         $hFin = $concert["heureFin"];
         echo ("<tr><th>" . $id . "</th><th>" . $idArtiste . "</th><th>" . $idScene . "</th><th>" . $date . "</th><th>" . $hDebut . "</th><th>" . $hFin . "</th><th>" . "<a href='index.php?action=modify&id=" . $id . "'>Modifier</a></th><th>" . "<a href='index.php?action=delete&id=" . $id . "'>Supprimer</a></th></tr>");
